3ca9548d9bbcda8a04f03fa82a14a835d8dfdf12
Kael Secrest <kael.secrest@icloud.com>
2026-01-23 10:30:05 -0700

    Adding first assinments and practice files

---
+++ Adv 01/PyAdv01.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+
+  Program: CSC115 Lab Adventure 01 
+ ----------------------------
+  Date: 1 / 13 / 2026
+  Partner 1 Name: Kael Secrest
+  Partner 1 Contributions:  
+
+  Partner 2 Name: 
+  Partner 2 Contributions: 
+   
+      
+      
+  Tips:
+    1. Read the entire Lab PDF so you understand what you need to do
+    2. Be sure to use comments and follow our coding standards.
+        * Not too many comments but enough to explain what you are doing.
+        * In this first lab I will provide some comments for you to follow
+    3. Use descriptive variable names
+    4. Display calculations with units
+    5. Do not let any lines (code or comments) go past 99 characters wife
+    6. Make sure your table output lines up
+    7. Rerun the code and test the output with different input values before submitting
+    8. DO NOT copy/paste from websites or GenAI.  Do your own work.
+      
+"""
+# Put constants here.  Make sure the variables are ALLCAPS
+# good constants are the company name, your university name, the AI name etc
+UseName = ""
+IDNum = ""
+SectorNum = ""
+PlanetName = ""
+
+# ------------------ Part 1: Introduction ----------------------------
+# TODO: Display a short narrative introducing the situation
+# Make it 3-5 sentences
+
+
+
+
+# ------------------ Part 2: HR System  ----------------------------
+# TODO: prompt the user for name, tempID, sector number and planet
+
+
+
+
+# ------------------ Part 3: Create Employee ID and calculate pay rates  ----------------------------
+# TODO: convert 4 digit temporary ID to 11 digit employee ID
+# TODO: Calculate pay rates for day, weekend, overtime, subspace and hyperspace
+
+
+
+
+# ------------------ Part 4: Output Employee HR Report ----------------------------
+# TODO: output an HR report with employee information and payrates
+# make sure all columns line up
+
+
+
+
+
+# ------------------ Part 5: Closing  ----------------------------
+# TODO: Display 2-3 sentences wrapping up the lab story
+
