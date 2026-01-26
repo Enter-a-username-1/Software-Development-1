91211f31019eb3d44ec1b2f4bb582a78f2ceaef1
Kael Secrest <kael.secrest@icloud.com>
2026-01-26 14:18:21 -0700

    chnages to lesson 4

--- Adv 01/PyAdv01.py
+++ Adv 01/PyAdv01.py
@@ -5,10 +5,10 @@
  ----------------------------
   Date: 1 / 13 / 2026
   Partner 1 Name: Kael Secrest
-  Partner 1 Contributions:  
+  Partner 1 Contributions: Code parts 1-4
 
   Partner 2 Name: 
-  Partner 2 Contributions: 
+  Partner 2 Contributions: Story, code part 5
    
       
       
@@ -35,30 +35,35 @@ PlanetName = ""
 # ------------------ Part 1: Introduction ----------------------------
 # TODO: Display a short narrative introducing the situation
 # Make it 3-5 sentences
-
+print("Welcome to Galactic HR Systems!")
+print("You have been hired as a new employee in the Galactic Federation.") 
 
 
 
 # ------------------ Part 2: HR System  ----------------------------
 # TODO: prompt the user for name, tempID, sector number and planet
-
+UseName = input("Enter your name: ")
+TempIDNum = str(input("Enter your temporary ID: "))
+SectorNum = input("Enter your sector number: ")
+PlanetName = input("Enter your planet name: ")
 
 
 
 # ------------------ Part 3: Create Employee ID and calculate pay rates  ----------------------------
 # TODO: convert 4 digit temporary ID to 11 digit employee ID
 # TODO: Calculate pay rates for day, weekend, overtime, subspace and hyperspace
-
-
-
+BackID = TempIDNum[3] + TempIDNum[2] + TempIDNum[1] + TempIDNum[0]
+IDNum = SectorNum + TempIDNum + SectorNum + str(BackID)
+CheckSum = int(IDNum[0]) + int(IDNum[1]) + int(IDNum[2]) + int(IDNum[3]) + int(IDNum[4]) + int(IDNum[5]) + int(IDNum[6]) + int(IDNum[7]) + int(IDNum[8]) + int(IDNum[9]) + int(IDNum[10])
 
 # ------------------ Part 4: Output Employee HR Report ----------------------------
 # TODO: output an HR report with employee information and payrates
 # make sure all columns line up
-
-
-
-
+print("Employee ID:", IDNum)
+print("Name:", UseName)
+print("Sector Number:", SectorNum)
+print("Planet Name:", PlanetName)
+print("CheckSum:", CheckSum)
 
 # ------------------ Part 5: Closing  ----------------------------
 # TODO: Display 2-3 sentences wrapping up the lab story
